7713ee89aee6b6ff963784159aac7a375feb27b4
ALREstevam <a166348@dac.unicamp.br>
2020-04-23 12:05:45 -0300

    data upadate

--- app/Data2.py
+++ app/Data2.py
@@ -12,7 +12,9 @@ class DataGenerator:
 
     def __init__(self, shelf='data', save_path='./'):
 
-        self.IBGE_URL = 'https://raw.githubusercontent.com/kelvins/Municipios-Brasileiros/master/csv/municipios.csv'
+        self.IBGE_URLS = ['https://raw.githubusercontent.com/ALREstevam/covid19-br-vis-back/04d236a58efcd60efb6717cf8226ceb63419555d/app/static/municipios.csv',
+                'https://raw.githubusercontent.com/kelvins/Municipios-Brasileiros/master/csv/municipios.csv'
+            ]
         self.IBGE_JSON = './app/static/ufcapitalIBGEcode.json'
         self.shelf = shelf
         self.save_path = save_path
@@ -42,13 +44,22 @@ class DataGenerator:
             },
         }
 
+
+    def df_ibge_download(self, capitals):
+        for url in self.IBGE_URLS:
+            try:
+                print(f'READING CITIES FROM {url}')
+                return DataGenerator.process_ibge_df(
+                    pd.read_csv(url), 
+                    capitals
+                )
+            except:
+                print('ERROR WHILE READING IBGE CSV')
+
     def generate_all(self, path='./app/static'):
         capitals = self.get_capitals()
 
-        df_ibge = DataGenerator.process_ibge_df(
-            pd.read_csv(self.IBGE_URL), 
-            capitals
-        )
+        df_ibge = self.df_ibge_download(capitals)
 
 
         def gen(url, to_data):
@@ -89,48 +100,68 @@ class DataGenerator:
         return lat, long
 
     def get_capitals(self):
+        print("GET CAPITALS")
         with open(self.IBGE_JSON) as json_file:
             return json.load(json_file)
 
     @staticmethod
     def process_ibge_df(df, capitals):
+        print("PROCESSING IBGE_DF")
         df.set_index('codigo_ibge', inplace=True)
         return df
 
-    
-
     @staticmethod
     def process_df(df, df_ibge, capitals):
-            print("PROCESSING DF")
-            lat, long = DataGenerator.lat_long_apply(df_ibge, capitals)
-
-            df = df [ df['state'] != 'TOTAL' ]
-
-            df.insert(len(df.columns), 'lat', np.nan)
-            df.insert(len(df.columns), 'long', np.nan)
-
-            df.loc[:, 'date'] = df.apply(lambda row: dateutil.parser.parse(row['date']), axis=1)
-            df.loc[:, 'lat'] = df.apply(lat, axis=1)
-            df.loc[:, 'long'] = df.apply(long, axis=1) 
-            
-            return df
+        print("PROCESSING DF")
+        lat, long = DataGenerator.lat_long_apply(df_ibge, capitals)
+        df = df [ df['state'] != 'TOTAL' ]
+        df.insert(len(df.columns), 'lat', np.nan)
+        df.insert(len(df.columns), 'long', np.nan)
+        df.loc[:, 'date'] = df.apply(lambda row: dateutil.parser.parse(row['date']), axis=1)
+        df.loc[:, 'lat'] = df.apply(lat, axis=1)
+        df.loc[:, 'long'] = df.apply(long, axis=1) 
+        
+        return df
 
     @staticmethod
     def df_to_data(df):
-            '''
-            Runs a function that generates a Pandas Dataframe and converts it to standard python data structures
-            that can be converted JSON
-            '''
+        '''
+        Runs a function that generates a Pandas Dataframe and converts it to standard python data structures
+        that can be converted JSON
+        '''
+        print('TO DATA JSON')
+        result = []
+        for index, row in df.iterrows():
+            result.append({
+                'date': row['date'],
+                'state': row['state'],
+                'city': row['city'].split('/')[0],
+                'ibgeID': str(row['ibgeID']),
+                'newDeaths': row['newDeaths'],
+                'deaths': row['deaths'],
+                'newCases': row['newCases'],
+                'totalCases': row['totalCases'],
+                'location': {
+                    'lat': row['lat'],
+                    'long': row['long'],
+                },
+            })
+        return result
 
-            print('TO DATA')
-            
-
-            result = []
-
-            for index, row in df.iterrows():
-
-                result.append({
+    @staticmethod
+    def df_to_geojson_data(df):
+        '''
+        Runs a function that generates a Pandas Dataframe and converts it to standard python data structures
+        that when converted to JSON are compliant with geojson standards 
+        '''
+        print('TO DATA GEOJSON')
+        features = []
+        for index, row in df.iterrows():
+            features.append({
+                'type': 'Feature',
+                'properties': {
                     'date': row['date'],
+                    'timestamp': int(row['date'].replace(tzinfo=timezone.utc).timestamp()) * 1000,
                     'state': row['state'],
                     'city': row['city'].split('/')[0],
                     'ibgeID': str(row['ibgeID']),
@@ -138,54 +169,21 @@ class DataGenerator:
                     'deaths': row['deaths'],
                     'newCases': row['newCases'],
                     'totalCases': row['totalCases'],
-                    'location': {
-                        'lat': row['lat'],
-                        'long': row['long'],
-                    },
-                })
-            return result
-
-    @staticmethod
-    def df_to_geojson_data(df):
-
-            '''
-            Runs a function that generates a Pandas Dataframe and converts it to standard python data structures
-            that when converted to JSON are compliant with geojson standards 
-            '''
-
-            print('TO DATA GEOJSON')
-
-            features = []
-
-            for index, row in df.iterrows():
-
-                features.append({
-                    'type': 'Feature',
-                    'properties': {
-                        'date': row['date'],
-                        'timestamp': int(row['date'].replace(tzinfo=timezone.utc).timestamp()) * 1000,
-                        'state': row['state'],
-                        'city': row['city'].split('/')[0],
-                        'ibgeID': str(row['ibgeID']),
-                        'newDeaths': row['newDeaths'],
-                        'deaths': row['deaths'],
-                        'newCases': row['newCases'],
-                        'totalCases': row['totalCases'],
-                    },
-                    'geometry':{
-                        'type': 'Point',
-                        'coordinates': [ row['long'], row['lat'] ],
-                    }
-                })
-
-            return {
-                'type': 'FeatureCollection',
-                'features': features 
+                },
+                'geometry':{
+                    'type': 'Point',
+                    'coordinates': [ row['long'], row['lat'] ],
+                }
+            })
+        return {
+            'type': 'FeatureCollection',
+            'features': features 
             }
 
     @staticmethod
     def generate(url, processor, to_data, ibge_df, capitals):
         def url2df(url):
+            print(f'DOWNLOAD {url}')
             return pd.read_csv(url)
 
         return to_data(
@@ -203,7 +201,7 @@ class DataGenerator:
 
 
 if __name__ == '__main__':
-    DataGenerator().generate_all()
+    DataGenerator('data', './app/static').generate_all()
 
 
 
