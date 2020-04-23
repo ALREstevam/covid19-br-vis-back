306bdb9333c9c2e3139f0f1263836a9f881c1725
ALREstevam <a166348@dac.unicamp.br>
2020-04-23 03:22:50 -0300

    prod tests

--- app/Data.py
+++ app/Data.py
@@ -23,10 +23,7 @@ IBGE_FILE = './app/static/municipios.csv'
 
 IBGE_JSON = './app/static/ufcapitalIBGEcode.json'
 
-cache = MemoryCache()
 
-def get_cached(key):
-    return cache.get(key)
 
 
 def lat_long_apply(df_ibge, capitals):
@@ -45,34 +42,7 @@ def lat_long_apply(df_ibge, capitals):
     return lat, long
 
 
-def ibge_data():
-        '''
-        Generates data related to IBGE's city id
-        '''
-        return get_cached('IBGE_DF'), get_cached('CAPITALS')
-        
 
-def process_ibge_df(df):
-    capitals = get_cached('CAPITALS')
-    df.set_index('codigo_ibge', inplace=True)
-    return df
-
-
-def process_df(df):
-        df_ibge, capitals = ibge_data()
-
-        lat, long = lat_long_apply(df_ibge, capitals)
-
-        df = df [ df['state'] != 'TOTAL' ]
-
-        df.insert(len(df.columns), 'lat', np.nan)
-        df.insert(len(df.columns), 'long', np.nan)
-
-        df.loc[:, 'date'] = df.apply(lambda row: dateutil.parser.parse(row['date']), axis=1)
-        df.loc[:, 'lat'] = df.apply(lat, axis=1)
-        df.loc[:, 'long'] = df.apply(long, axis=1) 
-        
-        return df
 
 def df_to_data(df):
         '''
@@ -133,61 +103,59 @@ def df_to_geojson_data(df):
             'features': features 
         }
 
-def generate_cities_time_df(self):
-    '''
-    Generates a Dataframe from `self.WCOTA_CSV`
-    '''
-    return self.default_df(self.WCOTA_CHANGES_ONLY_URL, self.WCOTA_CHANGES_ONLY_FILE)
-
-def generate_df(self):
-    '''
-    Generates a Datafraeme from `self.WCOTA_CITIES_TIME_CSV`
-    '''
-    return self.default_df(self.WCOTA_CITIES_TIME_URL, self.WCOTA_CITIES_TIME_FILE)
 
 
 
-def daily_geojson(self):
-    '''
-    Shortcut for getting the dataframe from `self.WCOTA_CITIES_TIME_CSV` as python's standard data structures 
-    that when converted to JSON are compliant with geojson standards
-    '''        
-    return df_to_geojson_data(self.generate_cities_time_df)
-    
 
-def daily_data(self):
-    '''
-    Shortcut for getting the dataframe from `self.WCOTA_CITIES_TIME_CSV` as python's standard data structures
-    '''        
-    return df_to_data(self.generate_cities_time_df)
+class WcotaCache:
 
-def cases_geojson(self):
-    '''
-    Shortcut for getting the dataframe from `self.WCOTA_CSV` as python's standard data structures 
-    that when converted to JSON are compliant with geojson standards
-    '''    
-    return df_to_geojson_data(self.generate_df)
-    
-def cases_data(self):
-    '''
-    Shortcut for getting the dataframe from `self.WCOTA_CSV` as python's standard data structures
-    ''' 
-    return df_to_data(self.generate_df)
+    def __init__(self):
+
+        with open(IBGE_JSON) as json_file:
+            capitals = json.load(json_file)
+
+        self.cache = MemoryCache()
+
+        self.cache.set('CAPITALS', capitals, 60*60*24*30)
 
 
-with open(IBGE_JSON) as json_file:
-    capitals = json.load(json_file)
+        dfs_data = [
+            df_data('IBGE_DF', IBGE_URL, -1,                                   self.process_ibge_df, lambda df: df),
+            df_data('WCOTA_CHANGES_JSON', WCOTA_CHANGES_ONLY_URL, 60*60*5,     self.process_df,      df_to_data),
+            df_data('WCOTA_CHANGES_GEOJSON', WCOTA_CHANGES_ONLY_URL, 60*60*5,  self.process_df,      df_to_geojson_data),
+            df_data('WCOTA_TIME_JSON', WCOTA_CITIES_TIME_URL, 60*60*5,         self.process_df,      df_to_data),
+            df_data('WCOTA_TIME_GEOJSON', WCOTA_CITIES_TIME_URL, 60*60*5,      self.process_df,      df_to_geojson_data),
+        ]
 
-cache.set('CAPITALS', capitals, 60*60*24*30)
+        thread = CsvDataManager(self.cache, update_cycle=10, cache_id='cache', dfs_data=dfs_data)
+        thread.start()
 
 
-dfs_data = [
-    df_data('IBGE_DF', IBGE_URL, -1, process_ibge_df, lambda df: df),
-    df_data('WCOTA_CHANGES_JSON', WCOTA_CHANGES_ONLY_URL, 60*60*5, process_df, df_to_data),
-    df_data('WCOTA_CHANGES_GEOJSON', WCOTA_CHANGES_ONLY_URL, 60*60*5, process_df, df_to_geojson_data),
-    df_data('WCOTA_TIME_JSON', WCOTA_CITIES_TIME_URL, 60*60*5, process_df, df_to_data),
-    df_data('WCOTA_TIME_GEOJSON', WCOTA_CITIES_TIME_URL, 60*60*5, process_df, df_to_geojson_data),
-]
+    def ibge_data(self):
+            '''
+            Generates data related to IBGE's city id
+            '''
+            return self.get_cached('IBGE_DF'), self.get_cached('CAPITALS')
 
-thread = CsvDataManager(cache, update_cycle=10, cache_id='cache', dfs_data=dfs_data)
-thread.start()
+
+    def get_cached(self, key):
+        return self.cache.get_key(key)
+
+
+    def process_ibge_df(self, df):
+        capitals = self.get_cached('CAPITALS')
+        df.set_index('codigo_ibge', inplace=True)
+        return df
+
+
+    def process_df(self, df):
+        df_ibge, capitals = self.ibge_data()
+
+        lat, long = lat_long_apply(df_ibge, capitals)
+        df = df [ df['state'] != 'TOTAL' ]
+        df.insert(len(df.columns), 'lat', np.nan)
+        df.insert(len(df.columns), 'long', np.nan)
+        df.loc[:, 'date'] = df.apply(lambda row: dateutil.parser.parse(row['date']), axis=1)
+        df.loc[:, 'lat'] = df.apply(lat, axis=1)
+        df.loc[:, 'long'] = df.apply(long, axis=1) 
+        return df
